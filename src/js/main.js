fa8d69881742607e58254d270e2987092ed3e5b0
NurlanPalvanAke <artyom.valeriyevich@gmail.com>
2022-12-30 16:17:30 +0600

    first project

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+
+
+
+
+let loginBtn = document.querySelector('#login')
+let login = document.querySelector('.login')
+let loginExit = document.querySelector('.login__form-exit')
+
+
+
+
+
+
+loginBtn.onclick = function () {
+    login.style.display = 'flex'
+    console.log('asd')
+}
+
+loginExit.onclick = function () {
+    login.style.display = 'none'
+}
+
+login.onclick = function (e) {
+    if (e.target.classList.contains('login')){
+        login.style.display = 'none'
+    }
+}
